03ad14e2dea05e24a6a6defd95e1358ee6734d3d
coderNexusGit <piciliano_g-unit@gmail.com>
2024-07-13 20:35:07 -0300

    feat: added new configs to bootstrap and app module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const configService = app.get<ConfigService>(ConfigService);
+
+  const docConfig = new DocumentBuilder()
+    .setTitle('Tech Repair Documentation.')
+    .setDescription('This documentation is about Tech repair app.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, docConfig);
+
+  SwaggerModule.setup('v1/docs', app, document);
+
+  app.setGlobalPrefix('v1/');
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(configService.get('APP_PORT') || 3003);
 }
 bootstrap();
